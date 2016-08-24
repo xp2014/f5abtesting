aef59a262df202d664a6f562b50340283d882fe4
Ping Xiong <p.xiong@f5.com>
2016-08-25 00:16:10 +0800

    The 1st version uploaded to pypi.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='f5abtesting',
     version='0.1.0',
     packages=['f5abtesting'],
+    package_data={'f5abtesting': ['*.conf']},
     url='https://github.com/xp2014/f5abtesting',
     license='Apache License, Version 2.0',
     author='Ping Xiong',
