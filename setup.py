e0fe4e72d7325dda0d832c9ebe25de204515ee1a
Ping Xiong <p.xiong@f5.com>
2016-08-26 00:07:10 +0800

    The 2st version uploaded to pypi, add install_requires for f5-sdk.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='f5abtesting',
-    version='0.1.0',
+    version='0.1.2',
     packages=['f5abtesting'],
     package_data={'f5abtesting': ['*.conf']},
+    install_requires=['f5-sdk >= 1.1.0'],
     url='https://github.com/xp2014/f5abtesting',
     license='Apache License, Version 2.0',
     author='Ping Xiong',
